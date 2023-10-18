ca36967b4ad8b9abfd980f93cbfc861af96193b5
waanpajaree <applekrnawee@gmail.com>
2023-10-18 14:07:52 +0700

    first commit

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import store from '@/store'
 export default () => {
     return axios.create({
-        baseURL: 'http://10.31.27.240:8081/',
+        baseURL: 'http://localhost:8081/',
         headers: {
             Authorization: `Bearer ${store.state.token}`
         }
